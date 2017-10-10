746e2ddd953212ef5a5e4c9034750fdf337335e0
Tom Tranter <t.g.tranter@uwaterloo.ca>
2017-10-10 09:11:54 -0400

    process all images in directory

--- examples/porous_image_2d.py
+++ examples/porous_image_2d.py
@@ -14,16 +14,32 @@ The sample file pipe.npy was generated using:
     (19.3 * (0.8 + 0.2 * np.sin(2 * pi * hx / 128.0)))**2] = True
 """
 
-import os
 import numpy as np
-
 from sailfish.subdomain import Subdomain2D
 from sailfish.node_type import NTFullBBWall
 from sailfish.controller import LBSimulationController
 from sailfish.lb_single import LBFluidSim, LBForcedSim
 import time
+import csv
+from scipy.misc import imread
 # No. neighbor interactions
+mypath='/home/tom/test_images/ml/Testing'
+from os import listdir
+from os.path import isfile, join
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+file=None
+all_perm = []
+all_tau = []
 
+def save_data(data=None):
+    with open('sailfish_data.csv', 'a', newline='\n') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csvwriter.writerow(data)
+# File Path
+save_data([mypath])
+# Header
+save_data(['file', 'porosity','perm', 'tau', 'iters', 'convergence'])
 
 class ExternalSubdomain(Subdomain2D):
     def initial_conditions(self, sim, hx, hy):
@@ -49,7 +65,12 @@ class ExternalSimulation(LBFluidSim, LBForcedSim):
     mu = 8.9e-4
     phi = 1.0
 
-
+    def read_jpeg(_file):
+        im_rgb = imread(_file)
+        im_r = im_rgb[:, :, 0]
+        solid = np.unique(im_r)[0]
+        return im_r == solid
+    
     @classmethod
     def add_options(cls, group, dim):
         group.add_argument('--geometry', type=str, default='pipe.npy',
@@ -60,15 +81,22 @@ class ExternalSimulation(LBFluidSim, LBForcedSim):
         defaults.update({
             'visc': cls.mu,
             'grid': 'D2Q9',
-            'max_iters': 30000,
+            'max_iters': 100000,
             'every': 250,
             'periodic_x': True})
 
     @classmethod
     def modify_config(cls, config):
+        global file
         if not config.geometry:
             return
-        config.geometry = np.load('/home/tom/test_images/test_blobs.npy')
+        try:
+            config.geometry = np.load(file)
+        except:
+            try:
+                config.geometry = cls.read_jpeg(file)
+            except:
+                print("FILE TYPE NOT VALID")
         # Override lattice size based on the geometry file.
         wall_map = ~config.geometry.astype(bool)
         cls.phi = 1-(np.sum(wall_map)/np.size(wall_map))
@@ -101,6 +129,7 @@ class ExternalSimulation(LBFluidSim, LBForcedSim):
         return tau
         
     def after_step(self, runner):
+        global file
         every_n = 250
         
         # Request the velocity field one step before actual processing.
@@ -119,36 +148,46 @@ class ExternalSimulation(LBFluidSim, LBForcedSim):
         if self.iteration % every_n == 0 and self.iteration > every_n:
             u_norm = self._u_norm(runner)
             du_norm = u_norm - self.u_old
-            convergence = np.abs(du_norm/u_norm)
+            self.convergence = np.abs(du_norm/u_norm)
             self.u_old = u_norm
             perm = self._calc_perm(runner)
             tau = self._calc_tau(runner)
             print('permeability', perm, 'tau', tau)
-            print('convergence', convergence)
+            print('convergence', self.convergence)
             self.data_table.append((self.iteration, du_norm, u_norm, perm, tau))
             # Crude convergence tactic
-            if convergence < 1e-5:
+            if self.convergence < 1e-5:
                 # Stop running on next iteration
                 # May be a better way to do this !!!
                 runner.config.max_iters = self.iteration + 1
 
         if self.iteration == self.config.max_iters - 1:
-            data_table_np = np.array(self.data_table)
-            np.savez('unorm',
-                     it=data_table_np[:, 0],
-                     du_norm=data_table_np[:, 1],
-                     u_norm=data_table_np[:, 2],
-                     perm=data_table_np[:, 3],
-                     tau=data_table_np[:, 4])
+#            data_table_np = np.array(self.data_table)
+#            np.savez('unorm',
+#                     it=data_table_np[:, 0],
+#                     du_norm=data_table_np[:, 1],
+#                     u_norm=data_table_np[:, 2],
+#                     perm=data_table_np[:, 3],
+#                     tau=data_table_np[:, 4])
+            print('SAVING DATA')
+            save_data([file.split('/')[-1].split('.')[0],
+                       str(self.phi),
+                       str(self._calc_perm(runner)),
+                       str(self._calc_tau(runner)),
+                       str(self.iteration),
+                       str(self.convergence)])
+            
 
     def __init__(self, config):
         super(ExternalSimulation, self).__init__(config)
         self.add_body_force((self.F, 0.0))
-        
 
 
 if __name__ == '__main__':
-    ctrl = LBSimulationController(ExternalSimulation)
-    st = time.time()
-    ctrl.run()
-    print('Sim time:', time.time()-st)
+    for f in onlyfiles:
+        file= join(mypath, f)
+        print('Processing', file)
+        ctrl = LBSimulationController(ExternalSimulation)
+        st = time.time()
+        ctrl.run()
+        print('Sim time:', time.time()-st)
